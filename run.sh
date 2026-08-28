1bab525a0d531a448ccec766dff28239f8fbe764
Lax <liulantao@gmail.com>
2026-08-28 10:29:42 +0800

    run.sh: use big-pickle model

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-opencode --model opencode/deepseek-v4-flash-free run 'bump version for all the packages & commit & push to github'
+opencode --model opencode/big-pickle run 'bump version for all the packages & commit & push to github'
